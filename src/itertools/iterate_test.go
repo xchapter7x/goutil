8d05e8eefe3d283e6786b4b085f5fe3676880e0e
John Calabrese <xchapter7x@gmail.com>
2013-11-27 22:44:53 -0500

    [#60746198] adding unit test for map
    we now have coverage for the iterate function
    for Map objects passed in

--- src/itertools/iterate_test.go
+++ src/itertools/iterate_test.go
@@ -15,6 +15,30 @@ func TearDownIterate() {
 }
 
 func Test_IterateMap(t *testing.T) {
+    controlSlice := make(map[int]string)
+    for i, v := range strings.Split("this is a test", "") {
+        controlSlice[i] = v
+    }
+	controlIndex := 0
+
+	for i := range Iterate(controlSlice) {
+        var first int
+        var second string
+        PairUnPack(i, &first, &second)
+
+		if second != controlSlice[first] {
+			t.Errorf("Index of iterate %d is not equal to control index of %d", second, controlSlice[first])
+		}
+
+        controlIndex++;
+	}
+
+	if controlIndex != len(controlSlice) {
+		t.Errorf("Number of iteratesion %d should be equal to string length %d", controlIndex, len(controlSlice))
+	}
+}
+
+func Test_IterateRing(t *testing.T) {
 
 }
 
@@ -122,8 +146,4 @@ func Test_IterateList(t *testing.T) {
 	if controlIndex != l.Len() {
 		t.Errorf("Number of iteratesion %d should be equal to string length %d", controlIndex, l.Len())
 	}
-}
-
-func Test_IterateRing(t *testing.T) {
-
 }
