d5c3d6dfa1b0a0d48498d9b734b93e803a0031c5
John Calabrese <jcalabrese@pivotal.io>
2014-12-10 18:45:51 -0500

    adding a callchain helper
    conditional statements all of the place
    are annoying...
    
    sometimes its unavoidable, until now

---
+++ itertools/callchain.go
@@ -0,0 +1,36 @@
+package itertools
+
+import "reflect"
+
+func createReflectValueArgsArray(iargs []interface{}) (args []reflect.Value) {
+	for _, arg := range iargs {
+		args = append(args, reflect.ValueOf(arg))
+	}
+	return
+}
+
+func createInterfaceArrayFromValuesArray(responseValuesArray []reflect.Value) (responseInterfaceArray []interface{}) {
+	for _, ri := range responseValuesArray {
+		responseInterfaceArray = append(responseInterfaceArray, ri.Interface())
+	}
+	return
+}
+
+func findErrorValue(responseInterfaceArray []interface{}) (err error) {
+	for _, res := range responseInterfaceArray {
+		if e, ok := res.(error); ok {
+			err = e
+		}
+	}
+	return
+}
+
+func CallChain(preverr error, functor interface{}, iargs ...interface{}) (responseInterfaceArray []interface{}, err error) {
+	if err = preverr; err == nil {
+		args := createReflectValueArgsArray(iargs)
+		responseValuesArray := reflect.ValueOf(functor).Call(args)
+		responseInterfaceArray = createInterfaceArrayFromValuesArray(responseValuesArray)
+		err = findErrorValue(responseInterfaceArray)
+	}
+	return
+}
