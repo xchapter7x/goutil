077e7a7f7c983f293c35a9243a3c5356719e549b
xchapter7x <xchapter7x@gmail.com>
2015-02-07 19:37:05 -0600

    more generic call to filter
    we can now pass more flexibly structured functors to filer.
    this means rather than our function being required to take
    2 args of type interface{}, we can define what the func
    signature is.
    
    within reason, as it will panic if you send a crazy signature

--- itertools/filter.go
+++ itertools/filter.go
@@ -5,22 +5,22 @@ import (
 	"sync"
 )
 
-func Filter(iter interface{}, f func(first, second interface{}) bool) (out chan Pair) {
+func Filter(iter interface{}, f interface{}) (out chan Pair) {
 	out = filter(passThrough, iter, f)
 	return
 }
 
-func CFilter(iter interface{}, f func(first, second interface{}) bool) (out chan Pair) {
+func CFilter(iter interface{}, f interface{}) (out chan Pair) {
 	out = cFilter(passThrough, iter, f)
 	return
 }
 
-func FilterFalse(iter interface{}, f func(first, second interface{}) bool) (out chan Pair) {
+func FilterFalse(iter interface{}, f interface{}) (out chan Pair) {
 	out = filter(falsify, iter, f)
 	return
 }
 
-func CFilterFalse(iter interface{}, f func(first, second interface{}) bool) (out chan Pair) {
+func CFilterFalse(iter interface{}, f interface{}) (out chan Pair) {
 	out = cFilter(falsify, iter, f)
 	return
 }
@@ -33,15 +33,45 @@ func passThrough(in bool) bool {
 	return in
 }
 
+func validateFunction(function reflect.Type) {
+
+	if function.Kind() != reflect.Func {
+		panic("not a func type")
+	}
+
+	if function.NumIn() > 2 {
+		panic("invalid argument count")
+	}
+
+	if function.NumOut() != 1 {
+		panic("invalid return value count")
+
+	} else {
+		res := function.Out(0)
+
+		if res.Kind() != reflect.Bool {
+			panic("response should be bool")
+		}
+	}
+}
+
 func pipeToFilterChannel(p Pair, out chan Pair, f interface{}, functor func(bool) bool) {
-	args := []reflect.Value{reflect.ValueOf(p.First), reflect.ValueOf(p.Second)}
+	function := reflect.TypeOf(f)
+	validateFunction(function)
+	pairValueArr := []reflect.Value{reflect.ValueOf(p.First), reflect.ValueOf(p.Second)}
+	args := []reflect.Value{}
+
+	for i := 0; i < function.NumIn(); i++ {
+		arg := pairValueArr[i].Convert(function.In(i))
+		args = append(args, arg)
+	}
 
 	if functor(reflect.ValueOf(f).Call(args)[0].Bool()) {
 		out <- p
 	}
 }
 
-func filter(functor func(bool) bool, iter interface{}, f func(first, second interface{}) bool) (out chan Pair) {
+func filter(functor func(bool) bool, iter interface{}, f interface{}) (out chan Pair) {
 	var wg sync.WaitGroup
 	out = make(chan Pair, GetIterBuffer())
 	wg.Add(1)
@@ -58,7 +88,7 @@ func filter(functor func(bool) bool, iter interface{}, f func(first, second inte
 	return
 }
 
-func cFilter(functor func(bool) bool, iter interface{}, f func(first, second interface{}) bool) (out chan Pair) {
+func cFilter(functor func(bool) bool, iter interface{}, f interface{}) (out chan Pair) {
 	var wg1 sync.WaitGroup
 	out = make(chan Pair, GetIterBuffer())
 	wg1.Add(1)
--- itertools/filter_test.go
+++ itertools/filter_test.go
@@ -1,8 +1,53 @@
-package itertools
+package itertools_test
 
 import (
+	"strings"
 	"testing"
+
+	. "github.com/xchapter7x/goutil/itertools"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
+
+var _ = Describe("Filter", func() {
+	var iterable []string = []string{
+		"hi",
+		"there",
+		"who are",
+		"you",
+		"that thing",
+	}
+	var output chan Pair
+
+	Context("called w/ non standard function format", func() {
+		BeforeEach(func() {
+			output = Filter(iterable, func(i int, v string) bool {
+				return strings.HasPrefix(v, "t")
+			})
+		})
+
+		It("should contain the correct number of elements", func() {
+			Ω(len(output)).Should(Equal(2))
+		})
+
+		It("should contain a value matches from the control iterable", func() {
+			val1 := <-output
+			val2 := <-output
+			Ω(arrayContains(val1.Second.(string), iterable)).Should(BeTrue())
+			Ω(arrayContains(val2.Second.(string), iterable)).Should(BeTrue())
+		})
+	})
+})
+
+func arrayContains(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
 
 var filterTestData []string
 var filterWhiteList []string
