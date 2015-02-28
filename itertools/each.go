ea755807b1022224d564b08512247319a6ca9a66
xchapter7x <xchapter7x@gmail.com>
2015-02-28 14:26:08 -0600

    a little more flexible Each/CEach
    we can now support variable structure functions
    passed into the Each.
    
    This gives us slightly more options as to what
    we can feed in, and hopefully avoid thin anonymous adapters

--- itertools/each.go
+++ itertools/each.go
@@ -1,28 +1,65 @@
 package itertools
 
 import (
+	"errors"
 	"reflect"
 	"sync"
 )
 
 func CEach(iter, f interface{}) {
-	var wg sync.WaitGroup
+	if err := validateEachFunction(f); err == nil {
+		var wg sync.WaitGroup
 
-	for p := range Iterate(iter) {
-		wg.Add(1)
+		for p := range Iterate(iter) {
+			wg.Add(1)
 
-		go func(pp Pair) {
-			defer wg.Done()
-			args := []reflect.Value{reflect.ValueOf(pp.First), reflect.ValueOf(pp.Second)}
-			reflect.ValueOf(f).Call(args)
-		}(p)
+			go func(pp Pair) {
+				defer wg.Done()
+				runEach(f, p)
+			}(p)
+		}
+		wg.Wait()
 	}
-	wg.Wait()
 }
 
 func Each(iter, f interface{}) {
-	for p := range Iterate(iter) {
-		args := []reflect.Value{reflect.ValueOf(p.First), reflect.ValueOf(p.Second)}
-		reflect.ValueOf(f).Call(args)
+	if err := validateEachFunction(f); err == nil {
+
+		for p := range Iterate(iter) {
+			runEach(f, p)
+		}
 	}
+}
+
+func runEach(f interface{}, p Pair) {
+	function := reflect.TypeOf(f)
+	pairArr := []interface{}{p.First, p.Second}
+	args := []reflect.Value{}
+
+	for i := 0; i < 2; i++ {
+		switch function.NumIn() {
+		case 1:
+			if reflect.TypeOf(pairArr[i]).ConvertibleTo(function.In(0)) {
+				arg := reflect.ValueOf(pairArr[i]).Convert(function.In(0))
+				args = []reflect.Value{arg}
+			}
+		case 2:
+			arg := reflect.ValueOf(pairArr[i]).Convert(function.In(i))
+			args = append(args, arg)
+		}
+	}
+	reflect.ValueOf(f).Call(args)
+}
+
+func validateEachFunction(f interface{}) (err error) {
+	function := reflect.TypeOf(f)
+
+	if function.Kind() != reflect.Func {
+		err = errors.New("not a func type")
+	}
+
+	if function.NumIn() > 2 {
+		err = errors.New("invalid argument count")
+	}
+	return
 }
--- itertools/each_test.go
+++ itertools/each_test.go
@@ -1,8 +1,51 @@
 package itertools
 
 import (
+	"strings"
 	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
+
+var _ = Describe("Filter", func() {
+	var iterable []string = []string{
+		"hi",
+		"there",
+		"who are",
+		"you",
+		"that thing",
+	}
+	var output int
+
+	Context("called w/ 2 typed argument signature", func() {
+		BeforeEach(func() {
+			output = 0
+			Each(iterable, func(i int, v string) bool {
+				output++
+				return strings.HasPrefix(v, "t")
+			})
+		})
+
+		It("should contain the correct number of elements", func() {
+			Ω(output).Should(Equal(len(iterable)))
+		})
+	})
+
+	Context("called w/ 1 typed argument signature", func() {
+		BeforeEach(func() {
+			output = 0
+			Each(iterable, func(v string) bool {
+				output++
+				return strings.HasPrefix(v, "t")
+			})
+		})
+
+		It("should contain the correct number of elements", func() {
+			Ω(output).Should(Equal(len(iterable)))
+		})
+	})
+})
 
 var (
 	f_called_each int
